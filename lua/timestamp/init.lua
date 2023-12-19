8a8ebfc805583569f7368230289fc4181a0efa24
George Kampanos <george.kampanos@nandos.co.uk>
2023-12-19 18:33:51 +0000

    refactor to use utils

--- lua/timestamp/init.lua
+++ lua/timestamp/init.lua
@@ -1,6 +1,8 @@
+local utils = require('timestamp.utils')
 local M = {}
 
 M.setup = function (opts)
+    for k, v in pairs(opts) do M[k] = v end
 end
 
 M.convert = function ()
@@ -11,18 +13,15 @@ M.convert = function ()
     end
 
     local opts = {}
-    local v_selection_start = vim.fn.getpos("v")  -- start of visual selection
-    local v_selection_end = vim.fn.getpos(".")  -- end of visual selection
+    local visual_coords = utils.get_visual_selection_coords()
 
-    P(v_selection_start)
-    P(v_selection_end)
+    P(visual_coords)
 
-    local start_row = v_selection_start[2] - 1
-    local start_col = v_selection_start[3] - 1
-    local end_row = v_selection_end[2] - 1
-    local end_col = v_selection_end[3] - 1
-
-    local text = vim.api.nvim_buf_get_text(0, start_row, start_col, end_row, end_col, opts)
+    local text = vim.api.nvim_buf_get_text(0,
+        visual_coords.start_row,
+        visual_coords.start_col,
+        visual_coords.end_row,
+        visual_coords.end_col, opts)
     P(text)
 
     return mode
---
+++ lua/timestamp/utils.lua
@@ -0,0 +1,30 @@
+local utils = {}
+
+function utils.get_visual_selection_coords()
+    local v_start = vim.fn.getpos("v")
+    local v_end = vim.fn.getpos(".")
+
+    local start_row = v_start[2] - 1
+    local start_col = v_start[3] - 1
+    local end_row = v_end[2] - 1
+    local end_col = v_end[3] - 1
+
+    -- need to check if the selection is reversed
+    -- if so, swap the start and end
+    if start_row > end_row then
+        start_row, end_row = end_row, start_row
+    end
+
+    if start_col > end_col then
+        start_col, end_col = end_col, start_col
+    end
+
+    return {
+        start_row = start_row,
+        start_col = start_col,
+        end_row = end_row,
+        end_col = end_col
+    }
+end
+
+return utils
