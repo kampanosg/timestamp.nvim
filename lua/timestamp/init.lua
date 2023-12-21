f68900313ce1edcfa544ca6ac23a8454fe397517
George Kampanos <george.kampanos@nandos.co.uk>
2023-12-21 15:31:10 +0000

    generate current formatted ts

--- lua/timestamp/init.lua
+++ lua/timestamp/init.lua
@@ -27,6 +27,25 @@ M.generate_formatted_static_timestamp = function ()
     utils.set_text(visual_coords, {timestamp})
 end
 
+M.generate_formatted_current_timestamp = function ()
+    local mode = vim.api.nvim_get_mode().mode
+    if mode ~= 'v' then
+        print('Please select text in visual mode')
+        return
+    end
+
+    local visual_coords = utils.get_visual_selection_coords()
+
+    local format = utils.get_text(visual_coords)
+    if #format == 0 then
+        print('Please select text in visual mode')
+        return
+    end
+
+    local timestamp = os.date(format[1])
+    utils.set_text(visual_coords, {timestamp})
+end
+
 M.generate_static_timestamp = function ()
     local timestamp = M.current_time(M.format)
     vim.api.nvim_put({timestamp}, 'c', true, true)
